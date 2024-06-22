e58d2dbde7f0257503ecc310ca51ddbb51661921
Sudhanshu Bhagwat <sudhanshubhagwat3@gmail.com>
2024-06-22 13:02:23 +0530

    Removed search as it is not implemented

--- src/app/(authenticated)/customers/page.tsx
+++ src/app/(authenticated)/customers/page.tsx
@@ -1,5 +1,4 @@
 import { DataTable } from "@/components/data-table";
-import { Input } from "@/components/ui/input";
 import {
   Sheet,
   SheetContent,
@@ -9,7 +8,7 @@ import {
   SheetTitle,
 } from "@/components/ui/sheet";
 import { getCustomers } from "@/services/customers";
-import { ArrowUpRight, PlusCircle, Search } from "lucide-react";
+import { ArrowUpRight, PlusCircle } from "lucide-react";
 import { columns } from "./components/columns";
 import { CreateCustomer } from "./components/create-customer";
 import Link from "next/link";
@@ -33,14 +32,6 @@ export default async function Page({
           </div>
         </div>
         <div className="flex gap-2">
-          <div className="relative ml-auto flex-1 md:grow-0">
-            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-            <Input
-              type="search"
-              placeholder="Search..."
-              className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[320px]"
-            />
-          </div>
           <Link
             href={"/customers?open=true"}
             className="cursor-pointer gap-2 bg-primary text-primary-foreground hover:bg-primary/90 h-9 px-3 inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background"
--- src/app/(authenticated)/invoices/page.tsx
+++ src/app/(authenticated)/invoices/page.tsx
@@ -1,6 +1,5 @@
 import { getInvoices } from "@/services/database";
-import { PlusCircle, Search } from "lucide-react";
-import { Input } from "@/components/ui/input";
+import { PlusCircle } from "lucide-react";
 import Link from "next/link";
 import { DataTable } from "@/components/data-table";
 import { columns } from "./components/columns";
@@ -18,14 +17,6 @@ export default async function Page() {
           </div>
         </div>
         <div className="flex gap-2">
-          <div className="relative ml-auto flex-1 md:grow-0">
-            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-            <Input
-              type="search"
-              placeholder="Search..."
-              className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[320px]"
-            />
-          </div>
           <Link
             href="/new-invoice"
             className="gap-2 bg-primary text-primary-foreground hover:bg-primary/90 h-9 px-3 inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background"
