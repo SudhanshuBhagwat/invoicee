0a1af573a8a221a761174fe583231243567952bb
Sudhanshu Bhagwat <sudhanshubhagwat3@gmail.com>
2025-10-24 18:54:38 +0530

    Added a basic react-router SPA and hooked up with Spring Boot app

---
+++ src/main/java/com/bhagwat/invoicee/configs/WebConfig.java
@@ -0,0 +1,20 @@
+package com.bhagwat.invoicee.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/")
+                .setCachePeriod(3900)
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver());
+    }
+
+}
