4cd577860cc7d2ee12d4ab1b1f0d21505ee45b39
Sudhanshu Bhagwat <sudhanshubhagwat3@gmail.com>
2025-10-24 17:28:43 +0530

    Initializing Invoicee with a new Spring Boot project

---
+++ src/main/java/com/bhagwat/invoicee/InvoiceeApplication.java
@@ -0,0 +1,13 @@
+package com.bhagwat.invoicee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhagwat/invoicee/InvoiceeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhagwat.invoicee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
